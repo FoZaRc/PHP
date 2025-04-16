02e7f4b19c90dd7c0f700873ff8cb55efa1d61e0
Fozar <fozarchik@gmail.com>
2025-04-16 16:31:52 +0300

    Сделал lab1

---
+++ lab1/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Гайворонский Семён 241-321 ЛБ1</title>
+</head>
+<body>
+    <header style="display: flex; justify-content: left; gap: 5%;">
+        <img src="./Logo_Polytech_rus_main.jpg" alt="Логотип" style="width: 30%; height: 20%;">
+        <h1>Домашняя работа: Hello, World!</h1>
+    </header>
+    <main>
+        <p><?php echo "Hello, world!" ?></p>
+        <p>
+            <?php echo date("l");?>
+        </p>
+    </main>
+    <footer>
+        <p>Какой-то подвал</p>
+    </footer>
+</body>
+</html>
+
